892ef315f75214ee82dafbfb4a0c506661965110
note38 <chardiegotis22003@gmail.com>
2022-03-11 17:14:29 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+let addToDobutton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
+
+addToDobutton.addEventListener ('click',function(){
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText=inputField.value;
+    toDoContainer.appendChild(paragraph);
+    inputField.value= "";
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration= "line-through";
+    })
+    paragraph.addEventListener('dblclick', function(){
+        toDoContainer.removeChild(paragraph);
+    })
+   
+})
+
+inputField.addEventListener('keydown', function(e){
+    if(e.keyCode === 13){ 
+        event.preventDefault();
+        document.getElementById('addToDo').click();
+    }
+    
+});
+   
